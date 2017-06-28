8155c810400460b40de42092bc0c5c4b38e9fa02
Enrico Belbusti <kigio88@hotmail.it>
2017-06-28 23:39:10 +0200

    fix state null su app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 import Test from './test';
 
-class App extends React.Component<{}, null> {
+class App extends React.Component<{}> {
   render() {
     return (
       <div className="App">
