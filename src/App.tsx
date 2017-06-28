83b23277630d0ede8fbe08b680b685f0fa70d4d6
Enrico Belbusti <loperto88@gmail.com>
2017-06-28 21:58:02 +0200

    project crated

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+import Test from './test';
+
+class App extends React.Component<{}, null> {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started <code>src/App.tsx</code> and save to reload.
+          <Test />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/test.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+export interface Props extends React.Props<any> {
+    start?: number | undefined;
+}
+
+class State {
+    interval: any;
+    counter: number = 0;
+}
+
+export default class Test extends React.Component<Props, State>{
+    constructor(props: Props) {
+        super(props);
+        this.state = new State();
+    }
+
+    componentDidMount() {
+        let interval = setInterval(this.updateCounter, 1000);
+        this.state = { ...this.state, interval };
+    }
+
+    componentWillUnmount() {
+        if (this.state.interval != undefined)
+            clearInterval(this.state.interval);
+    }
+
+    updateCounter = () => {
+        let counter = this.state.counter + 1;
+        this.setState({ counter });
+    }
+
+    render() {
+        return (
+            <span>{this.state.counter}</span>
+        );
+    }
+}
