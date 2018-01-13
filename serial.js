61149c3ee2d0e821f90a2052186ded94b1527390
Enrico Belbusti <loperto88@gmail.com>
2018-01-13 11:05:13 +0100

    fix serial read

--- serial.js
+++ serial.js
@@ -2,12 +2,13 @@ var SerialPort = require('serialport');
 
 module.exports = function Serial() {
     this.serial = new SerialPort('/dev/ttyACM0', {
-        baudRate: 9600
+        baudRate: 9600,        
     });
 
+    this.serial.on('open', () => console.log('serial port open'));
 
     this.serial.on('data', function (data) {
-        console.log('Data:', data);
+        console.log('Data:', data.toString());
     });
 
     this.write = function write(message, onError) {
