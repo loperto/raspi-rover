6f832b2f6a5e4249f3e3542e85298e377e054455
Enrico Belbusti <loperto88@gmail.com>
2018-02-03 17:43:29 +0100

    install font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 import ControlPanel from "./controlPanel";
 
 class App extends React.Component {
