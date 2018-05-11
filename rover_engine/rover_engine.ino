ff89a469b502ed542a8a9d4c51f2b3434a93bf9f
Enrico Belbusti <loperto88@gmail.com>
2018-05-12 01:48:15 +0200

    chenged rover engine layout

--- rover_engine/rover_engine.ino
+++ rover_engine/rover_engine.ino
@@ -10,69 +10,91 @@ Servo servoY;
 int posX = 90;
 int posY = 1;
 
-const int ledPin = 13;
-
-const int motorSpeed = 100;
+const int motorSpeed = 150;
 //CH1 right side
-const int ch1DirPin = 3;
-const int ch1CurPin = 4;
-const int ch1Pwm = 5;
+const int ch1DirectionPin = 4;
+const int ch1PwmPin = 5;
 //CH2 left side
-const int ch2DirPin = 7;
-const int ch2CurPin = 8;
-const int ch2Pwm = 6;
+const int ch2DirectionPin = 7;
+const int ch2PwmPin = 6;
 
-#define TRIGGER_PIN  12
-#define ECHO_PIN     11
+const int ledsPin = 13;
+const int buzzerPin = A0;
+
+#define TRIGGER_PIN 12
+#define ECHO_PIN 11
 #define MAX_DISTANCE 100
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-void setup() {
+void setup()
+{
 	Serial.begin(115200);
-	pinMode(ledPin, OUTPUT);
-	pinMode(ch1DirPin, OUTPUT);
-	pinMode(ch1CurPin, OUTPUT);
-	pinMode(ch1Pwm, OUTPUT);
-	pinMode(ch2DirPin, OUTPUT);
-	pinMode(ch2CurPin, OUTPUT);
-	pinMode(ch2Pwm, OUTPUT);
+	pinMode(ledsPin, OUTPUT);
+
+	pinMode(ch1DirectionPin, OUTPUT);
+	pinMode(ch1PwmPin, OUTPUT);
+
+
+	pinMode(ch2DirectionPin, OUTPUT);
+	pinMode(ch2PwmPin, OUTPUT);
+
+	pinMode(buzzerPin, OUTPUT);
+
 	servoX.attach(9);
 	servoY.attach(10);
 	servoX.write(posX);
 	servoY.write(posY);
+
+
+	//analogWrite(5, 100);
+	//analogWrite(6, 100);
+
+	/*beep(1000);*/
 }
 
-void moveCameraX(int degrees) {
-	if (degrees < 0 || degrees > 180 || degrees == posX) {
+void moveCameraX(int degrees)
+{
+	if (degrees < 0 || degrees > 180 || degrees == posX)
+	{
 		return;
 	}
-	else if (posX < degrees) {
-		for (posX; posX <= degrees; posX += 1) {
+	else if (posX < degrees)
+	{
+		for (posX; posX <= degrees; posX += 1)
+		{
 			servoX.write(posX);
 			delay(20);
 		}
 	}
-	else if (posX > degrees) {
-		for (posX; posX >= degrees; posX -= 1) {
+	else if (posX > degrees)
+	{
+		for (posX; posX >= degrees; posX -= 1)
+		{
 			servoX.write(posX);
 			delay(20);
 		}
 	}
 }
 
-void moveCameraY(int degrees) {
-	if (degrees < 0 || degrees > 180 || degrees == posY) {
+void moveCameraY(int degrees)
+{
+	if (degrees < 0 || degrees > 180 || degrees == posY)
+	{
 		return;
 	}
-	else if (posY < degrees) {
-		for (posY; posY <= degrees; posY += 1) {
+	else if (posY < degrees)
+	{
+		for (posY; posY <= degrees; posY += 1)
+		{
 			servoY.write(posY);
 			delay(20);
 		}
 	}
-	else if (posY > degrees) {
-		for (posY; posY >= degrees; posY -= 1) {
+	else if (posY > degrees)
+	{
+		for (posY; posY >= degrees; posY -= 1)
+		{
 			servoY.write(posY);
 			delay(20);
 		}
@@ -80,120 +102,148 @@ void moveCameraY(int degrees) {
 	return;
 }
 
-
-void toggleLed() {
-	int ledStatus = digitalRead(ledPin);
-	if (ledStatus == HIGH) {
-		digitalWrite(ledPin, LOW);
+void toggleLed()
+{
+	int ledStatus = digitalRead(ledsPin);
+	if (ledStatus == HIGH)
+	{
+		digitalWrite(ledsPin, LOW);
 	}
-	else {
-		digitalWrite(ledPin, HIGH);
+	else
+	{
+		digitalWrite(ledsPin, HIGH);
 	}
-
 }
 
-void ch1(int speed, bool forward) {
-	analogWrite(ch1Pwm, speed);
-	digitalWrite(ch1DirPin, forward);
-	digitalWrite(ch1CurPin, HIGH);
+void ch1(int speed, bool forward)
+{
+	analogWrite(ch1PwmPin, speed);
+	digitalWrite(ch1DirectionPin, !forward);
 }
 
-void ch2(int speed, bool forward) {
-	analogWrite(ch2Pwm, speed);
-	digitalWrite(ch2DirPin, forward);
-	digitalWrite(ch2CurPin, HIGH);
+void ch2(int speed, bool forward)
+{
+	analogWrite(ch2PwmPin, speed);
+	digitalWrite(ch2DirectionPin, forward);
 }
 
-void forward() {
+void forward()
+{
 	ch1(motorSpeed, true);
 	ch2(motorSpeed, true);
 }
 
-void backward() {
+void backward()
+{
 	ch1(motorSpeed, false);
 	ch2(motorSpeed, false);
 }
 
-void left() {
+void left()
+{
 	ch1(motorSpeed / 2, true);
 	ch2(motorSpeed / 2, false);
 }
 
-void right() {
+void right()
+{
 	ch1(motorSpeed / 2, false);
 	ch2(motorSpeed / 2, true);
 }
 
-void engineStop() {
+void engineStop()
+{
 	ch1(0, true);
 	ch2(0, true);
 }
 
+void beep(unsigned int duration)
+{
+	tone(buzzerPin, 1000, duration);
+	delay(duration);
+	noTone(buzzerPin);
+}
 
-void execCommand(const char* type, int value) {
-	if (strcmp(type, "forward") == 0) {
+void execCommand(const char *type, int value)
+{
+	if (strcmp(type, "forward") == 0)
+	{
 		forward();
 	}
-	else if (strcmp(type, "backward") == 0) {
+	else if (strcmp(type, "backward") == 0)
+	{
 		backward();
 	}
-	else if (strcmp(type, "left") == 0) {
+	else if (strcmp(type, "left") == 0)
+	{
 		left();
 	}
-	else if (strcmp(type, "right") == 0) {
+	else if (strcmp(type, "right") == 0)
+	{
 		right();
 	}
-	else if (strcmp(type, "stop") == 0) {
+	else if (strcmp(type, "stop") == 0)
+	{
 		engineStop();
 	}
-	else if (strcmp(type, "cameraX") == 0) {
+	else if (strcmp(type, "cameraX") == 0)
+	{
 		moveCameraX(value);
 	}
-	else if (strcmp(type, "cameraY") == 0) {
+	else if (strcmp(type, "cameraY") == 0)
+	{
 		moveCameraY(value);
 	}
-	else if (strcmp(type, "led") == 0) {
+	else if (strcmp(type, "beep") == 0)
+	{
+		beep(value);
+	}
+	else if (strcmp(type, "led") == 0)
+	{
 		toggleLed();
 	}
-	else {
+	else
+	{
 		Serial.print(type);
 		Serial.println(" command not found!");
 	}
 }
 
-
-void sendTelemetry() {
+void sendTelemetry()
+{
 	StaticJsonBuffer<200> jsonBuffer;
-	JsonObject& root = jsonBuffer.createObject();
-	root["s"] = "gps";
+	JsonObject &root = jsonBuffer.createObject();
 	root["dist"] = sonar.ping_cm();
 	root.printTo(Serial);
 	Serial.println();
 }
 
-
-void loop() {
-	if (Serial.available()) {
+void loop()
+{
+	if (Serial.available())
+	{
 		StaticJsonBuffer<200> jsonBuffer;
-		JsonObject& root = jsonBuffer.parse(Serial);
-		if (!root.success()) {
+		JsonObject &root = jsonBuffer.parse(Serial);
+		if (!root.success())
+		{
 			Serial.println("json parse failed");
 			return;
 		}
-		else {
-			const char* type = root["type"];
+		else
+		{
+			const char *type = root["type"];
 			int value = root["value"];
 			Serial.println(type);
 			Serial.println(value);
 			execCommand(type, value);
 		}
 	}
-	else {
-		if ((millis() - lastTelemetrySend) > telemetryFrequency) {
+	else
+	{
+		if ((millis() - lastTelemetrySend) > telemetryFrequency)
+		{
 			sendTelemetry();
 			lastTelemetrySend = millis();
 		}
 	}
-
-
 }
