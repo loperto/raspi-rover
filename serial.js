2ddce28742b85084e41e88829fb25ae60bfd4da0
Enrico Belbusti <loperto88@gmail.com>
2018-12-30 12:41:33 +0100

    aggiunto serial parser

--- serial.js
+++ serial.js
@@ -1,4 +1,6 @@
 var SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
 
 module.exports = function Serial(osName) {
     const port = osName === "Linux" ? "/dev/ttyACM0" : "COM4";
@@ -8,21 +10,23 @@ module.exports = function Serial(osName) {
         autoOpen: true,
     });
 
+    const parser = this.serial.pipe(new Readline({ delimiter: '\r\n' }))
+
     this.serial.on('open', () => console.log('serial port open'));
     this.serial.on('close', () => console.log('serial port closed'));
     this.onMessage = function onMessage(callback) {
         this.serial.on('data', (data) => {
-            message += data.toString();
-            let endIndex = message.indexOf("}");
-            if (endIndex !== -1) {
-                const cutted = message.substring(0, endIndex + 1);
-                console.log(cutted);
-                callback(cutted);
-                if (message.length - 1 === endIndex)
-                    message = "";
-                else
-                    message = message.substr(endIndex + 1, message.length - 1);
-            }
+            // message += data.toString();
+            // let endIndex = message.indexOf("}");
+            // if (endIndex !== -1) {
+            //     const cutted = message.substring(0, endIndex + 1);
+            console.log(data);
+            callback(data);
+            // if (message.length - 1 === endIndex)
+            //     message = "";
+            // else
+            //     message = message.substr(endIndex + 1, message.length - 1);
+            // }
         });
     }
 
