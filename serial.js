88581053d95bd0d7d54648c05362515d036e855a
Enrico Belbusti <loperto88@gmail.com>
2018-12-31 12:01:08 +0100

    serial  converted to class

--- serial.js
+++ serial.js
@@ -1,41 +1,34 @@
-var SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
+"use strict";
 
-module.exports = function Serial(osName) {
-    const port = osName === "Linux" ? "/dev/ttyACM0" : "COM4";
-    let message = "";
-    this.serial = new SerialPort(port, {
-        baudRate: 115200,
-        autoOpen: true,
-    });
+var SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
 
-    const parser = this.serial.pipe(new Readline({ delimiter: '\r\n' }))
+class Serial {
+    constructor(osName) {
+        const port = osName === "Linux" ? "/dev/ttyACM0" : "COM4";
+        this.serial = new SerialPort(port, {
+            baudRate: 115200,
+            autoOpen: true,
+        });
 
-    this.serial.on('open', () => console.log('serial port open'));
-    this.serial.on('close', () => console.log('serial port closed'));
-    this.onMessage = function onMessage(callback) {
+        this.parser = this.serial.pipe(new Readline({ delimiter: '\r\n' }));
+        this.serial.on('open', () => console.log('serial port open'));
+        this.serial.on('close', () => console.log('serial port closed'));
+        this.onMessage = this.onMessage.bind(this);
+        this.write = this.write.bind(this);
+        this.close = this.close.bind(this);
+    }
+
+    onMessage(callback) {
         parser.on('data', (data) => {
-            // message += data.toString();
-            // let endIndex = message.indexOf("}");
-            // if (endIndex !== -1) {
-            //     const cutted = message.substring(0, endIndex + 1);
             const readable = data.toString();
-            console.log(readable);
+            // console.log(readable);
             callback(readable);
-            // if (message.length - 1 === endIndex)
-            //     message = "";
-            // else
-            //     message = message.substr(endIndex + 1, message.length - 1);
-            // }
         });
     }
 
-    this.close = function close() {
-        this.serial.close();
-    }
-
-    this.write = function write(data, onError) {
-        this.serial.write(data, function (err) {
+    write(data, onError) {
+        this.serial.write(data, (err) => {
             if (err) {
                 console.log('error on message', data);
                 onError();
@@ -43,4 +36,10 @@ module.exports = function Serial(osName) {
             console.log('message sended');
         });
     }
-};
+
+    close() {
+        this.serial.close();
+    }
+}
+
+module.exports = Serial;
