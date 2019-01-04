426091833e7994c1c30a2a67780c4ba79ad6228a
Enrico Belbusti <loperto88@gmail.com>
2019-01-04 14:00:36 +0100

    inclination monitor wih font awesome

--- client/src/controlPanel/inclinationMonitor.tsx
+++ client/src/controlPanel/inclinationMonitor.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-let car = require("../car.png");
-let carFront = require("../car-front.png");
 
 export interface Props {
     pitch: number;
@@ -12,13 +10,13 @@ export default class InclinationMonitor extends React.PureComponent<Props>{
         const { pitch, roll } = this.props;
         return (
             <div>
-                <img
-                    src={car}
-                    style={{ width: 48, transform: `rotate(${pitch && pitch.toFixed(0) || 0}deg)` }}
+                <i
+                    className="fas fa-car-side fa-lg"
+                    style={{ transform: `rotate(${pitch && pitch.toFixed(0) || 0}deg)` }}
                 />
-                <img
-                    src={carFront}
-                    style={{ width: 48, transform: `rotate(${roll && roll.toFixed(0) || 0}deg)` }}
+                <i
+                    className="fas fa-car fa-lg"
+                    style={{ transform: `rotate(${roll && roll.toFixed(0) || 0}deg)` }}
                 />
             </div>
         );
