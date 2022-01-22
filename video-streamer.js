c5283692e5cfca10ed04660fdf40886518b32f8b
loperto <loperto88@gmail.com>
2022-01-22 22:40:42 +0100

    rotation

--- video-streamer.js
+++ video-streamer.js
@@ -41,10 +41,7 @@ class VideoStreamer {
 
     get_stream() {
         this.running = true;
-        var msk = "raspivid -t 0 -o - -n -w %d -h %d -fps %d";
-        var cmd = util.format(msk, this.options.width, this.options.height, this.options.fps);
-        console.log("exe command: ", cmd);
-        this.streamer = spawn('raspivid', ['-t', '0', '-o', '-', '-n', '-w', this.options.width, '-h', this.options.height, '-fps', this.options.fps, '-pf', 'baseline']);
+        this.streamer = spawn('raspivid', ['-t', '0', '-o', '-', '-n', '-rot', '180', '-w', this.options.width, '-h', this.options.height, '-fps', this.options.fps, '-pf', 'baseline']);
         this.streamer.on("exit", function (code) {
             if (code)
                 console.log(`Video streamer failure. Error code: ${code}`);
